68fdab6bc53c56b90e485b9ffe378d3e8813b9af
Fenteale <mangmasta@gmail.com>
2022-11-17 18:16:15 -0800

    Teleop code should be good to test now.

--- src/drivetrain_subscriber/drivetrain_subscriber/drivetrain.py
+++ src/drivetrain_subscriber/drivetrain_subscriber/drivetrain.py
@@ -2,8 +2,10 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Joy
+from std_msgs.msg import UInt8MultiArray
 
 class DrivetrainSubscriber(Node):
+	
 	def __init__(self):
 		super().__init__('drivetrain_subscriber')
 		self.subscription = self.create_subscription(
@@ -12,10 +14,36 @@ class DrivetrainSubscriber(Node):
 			self.listener_callback,
 			10)
 		self.subscription
+		self.publisher = self.create_publisher(UInt8MultiArray, 'motor_control', 10)
+		self.max_speed_val = 180
+		self.middle_speed_val = 90	
 	def listener_callback(self, msg):
-		print("Forward back:", msg.axes[1]) # forward back axis
-		print("Left right:", msg.axes[0]) # left right axis
+		#print("Forward back:", msg.axes[1]) # forward back axis
+		#print("Left right:", msg.axes[0]) # left right axis
+		self.left_speed = 0
+		self.right_speed = 0
+		self.left_speed = msg.axes[1] - msg.axes[0]
+		self.right_speed = msg.axes[1] + msg.axes[0]
+		if self.left_speed > 1:
+			self.left_speed = 1
+		elif self.left_speed < -1:
+			self.left_speed = -1
+		if self.right_speed > 1:
+			self.right_speed = 1
+		elif self.right_speed < -1:
+			self.right_speed = -1
+		self.left_speed = (self.left_speed + 1) * self.middle_speed_val
+		self.right_speed = (self.right_speed + 1) * self.middle_speed_val 
+		print("Left speed:", self.left_speed)
+		print("Right speed:", self.right_speed)
+		print("test")
+		self.msg_to_send = UInt8MultiArray()
+		print("Another test")
+		self.msg_to_send.data = [int(self.left_speed), int(self.right_speed)]
 		
+		print(self.msg_to_send)
+		self.publisher.publish(self.msg_to_send)
+
 		#print(msg)
 		
 def main(args=None):
